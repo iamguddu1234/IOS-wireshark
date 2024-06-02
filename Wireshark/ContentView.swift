7138e6faf671069736aaaa95371be0b86d7e5d82
Akshay Bhamse <iamguddu37@gmail.com>
2024-06-02 16:48:42 +0530

    Initial Commit

---
+++ Wireshark/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wireshark
+//
+//  Created by Akshay Bhasme on 02/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wireshark/WiresharkApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiresharkApp.swift
+//  Wireshark
+//
+//  Created by Akshay Bhasme on 02/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WiresharkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
